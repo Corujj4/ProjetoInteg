12f27fa5ebac40721e712242d511b7b22b879568
Corujj4 <raphaelsiqueiradesouza@gmail.com>
2024-12-10 10:57:22 -0300

    home

--- projetoint/lista/models.py
+++ projetoint/lista/models.py
@@ -1,10 +1,19 @@
 from django.db import models
 
+from listaprodutos.models import ListaProdutos
+
+
 # Create your models here.
 
 class Lista(models.Model):
     nome = models.CharField('nome',max_length=100,help_text='nome do produto')
     produto = models.ManyToManyField('produtos.Produtos', through='listaprodutos.ListaProdutos')
+
+
+    @property
+    def produtos(self):
+        return ListaProdutos.objects.filter(lista=self)
+
 
     class Meta:
         verbose_name = 'Lista'
